c0016a41e6bd33828f1c7d8d685bfe1ded2f9356
Sanu <sanu.bhumca2014@gmail.com>
2024-03-27 11:10:09 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# nodeJS-practice
+# Why Node web server?
+  - Heavy I/O
+  - Small code footprint
+
+# Modules
+    - HTTP
+    - FileSystem
+    - OS
---
+++ index.js
@@ -0,0 +1,15 @@
+// import {sum,diff} from './lib.js'
+// const fs = require('fs')
+const express = require('express')
+
+
+const server = express()
+server.listen(8000)
+
+// const txt = fs.readFileSync('./demo.txt','utf-8')
+// console.log(performance.now())
+// const txt = fs.readFile('./demo.txt','utf-8',(err,txt)=>{
+//     console.log(txt)
+// })
+// console.log(performance.now())
+
